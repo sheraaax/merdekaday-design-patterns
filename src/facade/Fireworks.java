5e48504a9c1c8971cb79f8be6d6313cf261f7c6f
Amirah <amirahrozey@gmail.com>
2020-12-09 11:21:51 +0800

    Add println in Fireworks.java

--- src/facade/Fireworks.java
+++ src/facade/Fireworks.java
@@ -15,8 +15,6 @@ public class Fireworks
         this.fireworks1=fireworks1;
         this.fireworks2=fireworks2;
         this.fireworks3=fireworks3;
-        // this.orn4=orn4;
-        // this.orn5=orn5;
     }
     
     public void setFireworks()
@@ -26,8 +24,7 @@ public class Fireworks
             fireworks1.setVisible(false);
             fireworks2.setVisible(false);
             fireworks3.setVisible(false);
-            // orn4.setVisible(false);
-            // orn5.setVisible(false);
+            
             showfireworks=false;
             System.out.println("No fireworks are displayed!");
         }
@@ -36,8 +33,7 @@ public class Fireworks
             fireworks1.setVisible(true);
             fireworks2.setVisible(true);
             fireworks3.setVisible(true);
-            // orn4.setVisible(true);
-            // orn5.setVisible(true);
+            
             showfireworks=true;
             System.out.println("All fireworks are displayed!");
         }
